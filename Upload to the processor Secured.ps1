e598d16f1f5df154aa1277450cf5be2a70ccaaf3
Filip Horvat <40072838+raut3r@users.noreply.github.com>
2022-04-19 18:22:35 +0200

    Added Crestron PS scripts

---
+++ Upload to the processor Secured.ps1
@@ -0,0 +1,14 @@
+﻿# variables
+$file = # Path to the cpz file
+# e.g. $file = "C:\Project\Backend\bin\Debug\projectName.cpz"
+$ip = # 
+# e.g. $ip = "192.168.1.110"
+$slotNumber = # Enter the slot number
+# e.g. $slotNumber = 1
+$username = # username in the quotation marks
+# e.g. $username = "admin"
+$password = # password in the quotation marks
+# e.g. $password = "admin"
+
+#command
+Send-CrestronProgram -Device $ip -LocalFile $file -Password $password -ProgramSlot $slotNumber -Secure -Username $username
---
+++ Upload to the processor Unsecured.ps1
@@ -0,0 +1,10 @@
+﻿# variables
+$file = # Path to the cpz file
+# e.g. $file = "C:\Project\Backend\bin\Debug\projectName.cpz"
+$ip = # 
+# e.g. $ip = "192.168.1.110"
+$slotNumber = # Enter the slot number
+# e.g. $slotNumber = 1
+
+#command
+Send-CrestronProgram -Device $ip -LocalFile $file -ProgramSlot $slotNumber -Secure
